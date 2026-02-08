2d4d108737c13d474b1c61d2ab44268cf5fccf59
deaklevi <deakleventevok@gmail.com>
2026-02-08 20:20:12 +0100

    CREATE levente_frontend vue project (axios, pinia, tailwind css, auto_router) and done set up

---
+++ levente_frontend/src/layouts/BaseLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <BaseHeader />
+  <main class="mx-auto  max-w-7xl">
+    <slot />
+  </main>
+</template>
+<script>
+import BaseHeader from '@components/layout/BaseHeader.vue'
+
+export default {
+  components: {
+    BaseHeader
+  }
+}
+</script>
